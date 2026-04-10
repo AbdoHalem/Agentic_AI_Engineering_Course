93d62d7d8563dfbd662fb71773ce682c8fee793d
Abdo_Halem10 <abdohalem305@gmail.com>
2026-04-10 17:22:56 +0200

    Return the agent's answer in a structured response using pydantic object.

--- tavily_tool.py
+++ tavily_tool.py
@@ -5,9 +5,23 @@ from langchain_core.messages import HumanMessage
 # Import the Google GenAI chat model instead of OpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
-
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
+
+class Source(BaseModel):
+    """
+    Schema for a source used by the agent
+    """
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for agent response with answer and sources
+    """
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
 
@@ -29,11 +43,11 @@ search_tool = TavilySearch(
 )
 
 tools = [search_tool]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from agentic-ai-engineering-course!")
-    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]})
+    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Cairo?")]})
     print(result)
 
 if __name__ == "__main__":
